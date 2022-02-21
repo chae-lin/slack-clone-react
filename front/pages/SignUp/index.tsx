dee1c052f8c4f8c829f623780a18be1924db9312
chaelin kim <gjf0055@gmail.com>
2022-02-21 11:40:04 +0900

    feat: axios로 요청 보내기와 CORS, proxy

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import useInput from '@hooks/useInput';
 
@@ -10,6 +11,8 @@ const SignUp = () => {
   const [password, , setPassword] = useInput('');
   const [passwordCheck, , setPasswordCheck] = useInput('');
   const [mismatchError, setMismatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccese] = useState(false);
 
   const onChangePassword = useCallback(
     (e) => {
@@ -31,7 +34,28 @@ const SignUp = () => {
     (e) => {
       e.preventDefault();
       if (!mismatchError) {
-        console.log('서버로 회원가입');
+        // 비동기 요청에서 setState 하는 것들은 비동기 요청하기전에 초기화 해주는 것이 좋다.
+        // 그렇지 않으면 요청을 연달아 날릴 때 이전에 보낸 요청의 값이 남아있는 경우가 있을 수 있어 요청 보내기전에 초기화 해주는 것이 좋다.
+        setSignUpError('');
+        setSignUpSuccese(false);
+        axios
+          // webpack.congig에서 proxy 설정한 경우
+          // .post('/api/users', { email, nickname, password })
+          // 위의 경우는 3095에서 3095로 보내는 것.
+          // 아래의 경우는 3090에서 3095로 보내는 것
+          .post('http://localhost:3095/api/users', { email, nickname, password })
+          // 성공
+          .then((response) => {
+            setSignUpSuccese(true);
+            console.log(response);
+          })
+          // 실패
+          .catch((error) => {
+            setSignUpError(error.response.data);
+            console.log(error.response);
+          });
+        // 성공, 실패 모두 실행
+        // .finally(() => {});
       }
     },
     [email, nickname, password, passwordCheck],
@@ -72,8 +96,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {/*{signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
